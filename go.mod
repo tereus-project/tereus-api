56eb834d8984e0667c48d41cf771c1c5ee418302
Stanislas Lange <git@slange.me>
2022-03-06 15:13:18 +0100

    Handle source code upload and object storage
    Close https://github.com/tereus-project/tereus-api/issues/2

--- go.mod
+++ go.mod
@@ -2,13 +2,31 @@ module github.com/tereus-project/tereus-api
 
 go 1.17
 
-require github.com/labstack/echo/v4 v4.7.0
+require (
+	github.com/labstack/echo/v4 v4.7.0
+	github.com/minio/minio-go/v7 v7.0.23
+)
 
 require (
+	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/google/uuid v1.1.1 // indirect
+	github.com/gopherjs/gopherjs v0.0.0-20220221023154-0b2280d3ff96 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/jtolds/gls v4.20.0+incompatible // indirect
+	github.com/klauspost/compress v1.13.5 // indirect
+	github.com/klauspost/cpuid v1.3.1 // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/minio/md5-simd v1.1.0 // indirect
+	github.com/minio/sha256-simd v0.1.1 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/rs/xid v1.2.1 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/smartystreets/assertions v1.2.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
@@ -16,4 +34,5 @@ require (
 	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
+	gopkg.in/ini.v1 v1.57.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"archive/zip"
+	"context"
+	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
 )
+
+var minioClient *minio.Client
 
 func main() {
 	// Echo instance
@@ -17,6 +26,17 @@ func main() {
 
 	// Routes
 	e.GET("/", hello)
+	e.POST("/remix/:src/to/:target", remix)
+
+	// Initialize minio client object.
+	var err error
+	minioClient, err = minio.New("localhost:9000", &minio.Options{
+		Creds:  credentials.NewStaticV4("miniokey", "miniosecret", ""),
+		Secure: false,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
@@ -25,4 +45,62 @@ func main() {
 // Handler
 func hello(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
+}
+
+type remixResult struct {
+	ID             string `json:"id"`
+	SourceLanguage string `json:"source_language"`
+	TargetLanguage string `json:"target_language"`
+}
+
+func remix(c echo.Context) error {
+	srcLanguage := c.Param("src")
+	targetLanguage := c.Param("target")
+
+	jobID := uuid.New().String()
+
+	file, err := c.FormFile("file")
+	if err != nil {
+		return err
+	}
+	src, err := file.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	zipReader, err := zip.NewReader(src, int64(file.Size))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range zipReader.File {
+		if file.FileInfo().IsDir() {
+			continue
+		}
+		f, err := file.Open()
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		log.Println(file.Name)
+		_, err = minioClient.PutObject(
+			context.Background(),
+			"tereus",
+			fmt.Sprintf("remix/%s/%s", jobID, file.Name),
+			f,
+			file.FileInfo().Size(),
+			minio.PutObjectOptions{},
+		)
+		if err != nil {
+			c.Logger().Error(err)
+			return c.String(http.StatusInternalServerError, "Internal Server Error")
+		}
+	}
+
+	return c.JSON(http.StatusOK, remixResult{
+		ID:             jobID,
+		SourceLanguage: srcLanguage,
+		TargetLanguage: targetLanguage,
+	})
 }
