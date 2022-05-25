7bc3018d7c6b44fa0ace6fd5511016c77cfbc81b
Stanislas Lange <git@slange.me>
2022-05-25 16:13:19 +0200

    Move DeleteSubmission to SubmissionsHandler

--- handlers/users.go
+++ handlers/users.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/sirupsen/logrus"
 	"github.com/tereus-project/tereus-api/ent"
@@ -106,63 +105,4 @@ func (h *UserHandler) GetSubmissionsHistory(c echo.Context) error {
 	}
 
 	return c.JSON(http.StatusOK, response)
-}
-
-// DELETE /submissions/:id
-func (h *UserHandler) DeleteSubmission(c echo.Context) error {
-	tereusUser, err := h.tokenService.GetUserFromContext(c)
-	if err != nil {
-		return err
-	}
-
-	submissionID, err := uuid.Parse(c.Param("id"))
-	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, "Invalid submission ID")
-	}
-
-	sub, err := h.databaseService.Submission.Get(context.Background(), submissionID)
-	if err != nil {
-		if ent.IsNotFound(err) {
-			return echo.NewHTTPError(http.StatusNotFound, "Submission not found")
-		}
-		logrus.WithError(err).Error("Failed to get submission")
-		return err
-	}
-
-	owner, err := sub.QueryUser().FirstID(c.Request().Context())
-	if err != nil {
-		logrus.WithError(err).Error("Failed to get submission owner")
-		return err
-	}
-
-	if owner != tereusUser.ID {
-		return echo.NewHTTPError(http.StatusForbidden, "You are not allowed to delete this submission")
-	}
-
-	// Already deleted, skip S3/DB deletion
-	if sub.Status == submission.StatusDeleted {
-		return c.NoContent(http.StatusNoContent)
-	}
-
-	// Delete from object storage
-	err = h.s3Service.DeleteSubmission(sub.ID.String())
-	if err != nil {
-		logrus.WithError(err).Error("Failed to delete submission from object storage")
-		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
-	}
-
-	// Set submission as deleted
-	err = h.databaseService.Submission.
-		Update().
-		Where(
-			submission.ID(sub.ID),
-		).
-		SetStatus(submission.StatusDeleted).
-		Exec(context.Background())
-	if err != nil {
-		logrus.WithError(err).Error("Failed to set submission as deleted")
-		return err
-	}
-
-	return c.NoContent(http.StatusNoContent)
 }
