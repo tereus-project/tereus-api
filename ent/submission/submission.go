2555e3902d76b168d6ba221c48620f65abfb0a67
Stanislas Lange <git@slange.me>
2022-05-25 16:16:44 +0200

    Submissions: rename deleted status to cleaned

--- ent/submission/submission.go
+++ ent/submission/submission.go
@@ -90,7 +90,7 @@ const (
 	StatusProcessing Status = "processing"
 	StatusDone       Status = "done"
 	StatusFailed     Status = "failed"
-	StatusDeleted    Status = "deleted"
+	StatusCleaned    Status = "cleaned"
 )
 
 func (s Status) String() string {
@@ -100,7 +100,7 @@ func (s Status) String() string {
 // StatusValidator is a validator for the "status" field enum values. It is called by the builders before save.
 func StatusValidator(s Status) error {
 	switch s {
-	case StatusPending, StatusProcessing, StatusDone, StatusFailed, StatusDeleted:
+	case StatusPending, StatusProcessing, StatusDone, StatusFailed, StatusCleaned:
 		return nil
 	default:
 		return fmt.Errorf("submission: invalid enum value for status field: %q", s)
